d752f5fa1d4cedf115ba3769a0537affc9bce47c
christopherpross <cpross.msh@blista.de>
2017-03-30 14:08:48 +0200

    30 Ausgabe hinzugefügt

--- egit hello world/src/helloworld/HelloWorld.java
+++ egit hello world/src/helloworld/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hallo Welt!");
 		System.out.print("2. ausgabe!");
-		System.out.println("3. Ausgabe");
+		System.out.println("4. Ausgabe");
 
 	}
 
